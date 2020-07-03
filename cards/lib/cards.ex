0f82bbc24a7b0f40f8c3b769880522ac23325ebf
Thiago Klein <thiago.klein@olxbr.com>
2020-07-03 13:40:18 -0300

    creating first module

---
+++ cards/lib/cards.ex
@@ -0,0 +1,55 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for `Cards`.
+  """
+
+  @doc """
+  Create deck.
+
+  ## Examples
+
+      iex> Cards.create_deck()
+      :[...]
+
+  """
+
+  def create_deck do
+    values =["Ace", "Two", "Three", "Four", "Five"]
+    # values =["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs"]
+    for suit <- suits, value <- values do
+        "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, file_name) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(file_name, binary)
+  end
+
+  def load(file_name) do
+    case File.read(file_name) do
+      { :ok, binary } -> :erlang.binary_to_term(binary)
+      { :error, _ } -> []
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+  end
+
+end
