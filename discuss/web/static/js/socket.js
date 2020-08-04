d957f0f0627fb6ea0584f640166ce1ff4158ad28
Thiago Klein <thiago.klein@olxbr.com>
2020-08-04 12:38:34 -0300

    using sockets to render comments

--- discuss/web/static/js/socket.js
+++ discuss/web/static/js/socket.js
@@ -53,10 +53,36 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 
 socket.connect()
 
-// Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("topic:subtopic", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+function commentTemplate(comment) {
+    return `<li class="collection-item">${comment.content}</li>`
 
-export default socket
+}
+
+function renderComments(comments) {
+  const renderedComments = comments.map(commentTemplate)
+  document.querySelector('.collection').innerHTML = renderedComments.join('')
+}
+
+function renderComment(comment) {
+  const renderedComment = commentTemplate(comment)
+  document.querySelector('.collection').innerHTML += renderedComment  
+}
+
+const createSocket = (topicId) => {
+let channel = socket.channel(`comments:${topicId}`, {})
+  channel.join()
+    .receive("ok", resp => { 
+      console.log("Joined successfully", resp)
+      renderComments(resp.comments)
+    })
+    .receive("error", resp => { console.log("Unable to join", resp) })
+
+  channel.on(`comments:${topicId}:new`, event => renderComment(event.comment))
+
+  document.querySelector('button').addEventListener('click', () => {
+    const content = document.querySelector('textarea').value
+    channel.push('comment:add', {content: content})
+  })
+}
+
+window.createSocket = createSocket;
